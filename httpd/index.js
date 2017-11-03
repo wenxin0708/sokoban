f83f2b8f66609625c402b3463c84bb17e3cb8d4f
wenxin0708 <wenxin9778@gmail.com>
2017-11-03 15:11:29 +0800

    ...

--- httpd/index.js
+++ httpd/index.js
@@ -1,17 +1,22 @@
 'use strict';
+
 let http = require('http');
-http.createServer((request, response) => {
-// 傳送 HTTP header
-// HTTP Status: 200 : OK
-// Content Type: text/plain
-response.writeHead(200, {
-'Content-Type': 'text/plain'
+ http.createServer((request, response) => {
+request.on('end', () => {
+ console.log('Request method: ' + request.method);
+ console.log('Request url: ' + request.url);
 });
 
-// 傳送回應內容。
-response.end('Hello World!\n');
-console.log('request.headers: \n', request.headers)
-}).listen(8088);
+ // 傳送 HTTP header
+ // HTTP Status: 200 : OK
+// Content Type: text/plain
+ response.writeHead(200, {
+'Content-Type': 'text/plain'
+ });
+
+ // 傳送回應內容。
+ response.end('Hello World!\n');
+ }).listen(8088);
 
 // log message to Console
-console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+ console.log('Server running at http://127.0.0.1:8088/')
